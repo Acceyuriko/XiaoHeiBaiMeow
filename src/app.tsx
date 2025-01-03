449613f63b50762f21e8780863539f27e95481ba
Acceyuriko <Acceyuriko@gmail.com>
2025-01-03 20:43:07 +0800

    feat: mouse-firework

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,45 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import initMouseFirework from 'mouse-firework';
 import { Toaster } from 'react-hot-toast';
 import { RouterProvider, createBrowserRouter, Outlet } from 'react-router-dom';
 
 import { useAppStore } from './store/app';
 import { CatLoading } from '@/components/cat-loading';
+
+initMouseFirework({
+  excludeElements: ['a'],
+  particles: [
+    {
+      shape: 'circle',
+      move: ['emit'],
+      easing: 'easeOutExpo',
+      colors: [
+        'rgba(255,182,185,.9)',
+        'rgba(250,227,217,.9)',
+        'rgba(187,222,214,.9)',
+        'rgba(138,198,209,.9)',
+      ],
+      number: 30,
+      duration: [1200, 1800],
+      shapeOptions: {
+        radius: [16, 32],
+      },
+    },
+    {
+      shape: 'circle',
+      move: ['diffuse'],
+      easing: 'easeOutExpo',
+      colors: ['#fff'],
+      number: 1,
+      duration: [1200, 1800],
+      shapeOptions: {
+        radius: 20,
+        alpha: 0.5,
+        lineWidth: 6,
+      },
+    },
+  ],
+});
 
 const queryClient = new QueryClient({
   defaultOptions: {

d179e5908af761c771a3c7e6e29126bb08531d34
Acceyuriko <Acceyuriko@gmail.com>
2025-01-24 23:35:02 +0800

    fix: 404 style

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -5,7 +5,7 @@ import Anime404 from './assets/404.json';
 export const Page404 = () => {
   return (
     <div className="flex items-center justify-center">
-      <Lottie animationData={Anime404} />
+      <Lottie className="my-10" animationData={Anime404} />
     </div>
   );
 };

35cefbcee4c1eff39411716513a376953ace6b83
Acceyuriko <Acceyuriko@gmail.com>
2025-03-02 23:17:14 +0800

    feat: tags

--- src/pages/tag/index.tsx
+++ src/pages/tag/index.tsx
@@ -1,3 +1,65 @@
+import dayjs from 'dayjs';
+import { useEffect, useMemo } from 'react';
+import { useParams, Link } from 'react-router';
+
+import { CatLoading } from '@/components/cat-loading';
+import { Timeline } from '@/components/timeline';
+import { useNotes } from '@/hooks/useNotes';
+import { useAppStore } from '@/store/app';
+
 export const Tag = () => {
-  return <div>Tag</div>;
+  const { tag } = useParams<{ tag: string }>();
+  const { setTitle, setSubTitle } = useAppStore();
+  const { data } = useNotes();
+
+  useEffect(() => {
+    setTitle(`包含标签“${tag}”的文章`);
+    setSubTitle(<span></span>);
+    return () => {
+      setTitle('');
+      setSubTitle(undefined);
+    };
+  }, [setTitle, setSubTitle, tag]);
+
+  const notes = useMemo(() => {
+    return (data?.notes.filter((i) => i.tags.includes(tag!)) || []).sort(
+      (a, b) => b.createdAt - a.createdAt,
+    );
+  }, [data, tag]);
+
+  if (!data) {
+    return (
+      <div className="relative h-80 w-full">
+        <CatLoading />
+      </div>
+    );
+  }
+
+  return (
+    <div className="mx-[5px] mb-5 animate-slide-up-big-in p-2 md:p-5">
+      <Timeline
+        data={notes.map((item) => {
+          return {
+            content: (
+              <div>
+                <span className="mr-2.5 text-[0.75em] text-grey-4">
+                  {dayjs(item.createdAt).format('MM-DD')}
+                </span>
+                <Link
+                  to={`/note/${item.title}`}
+                  className="cursor-pointer leading-[2] text-red hover:text-blue"
+                  style={{
+                    transition: 'all 0.2s ease-in-out',
+                  }}
+                >
+                  {item.title}
+                </Link>
+              </div>
+            ),
+            time: item.createdAt,
+          };
+        })}
+      />
+    </div>
+  );
 };
--- src/pages/tags/index.tsx
+++ src/pages/tags/index.tsx
@@ -1,3 +1,62 @@
+import { useEffect, useMemo } from 'react';
+import { useNavigate } from 'react-router';
+import { TagCloud } from 'react-tagcloud';
+
+import { CatLoading } from '@/components/cat-loading';
+import { useNotes } from '@/hooks/useNotes';
+import { useAppStore } from '@/store/app';
+
 export const Tags = () => {
-  return <div>Tags</div>;
+  const navigate = useNavigate();
+  const { setTitle, setSubTitle } = useAppStore();
+  const { data } = useNotes();
+
+  useEffect(() => {
+    setTitle('全部标签');
+    setSubTitle(<span></span>);
+    return () => {
+      setTitle('');
+      setSubTitle(undefined);
+    };
+  }, [setTitle, setSubTitle]);
+
+  const tags = useMemo(() => {
+    if (!data) {
+      return [] as { value: string; count: number }[];
+    }
+    const map = new Map<string, number>();
+    data.notes.forEach((i) => {
+      i.tags.forEach((j) => {
+        if (map.has(j)) {
+          map.set(j, map.get(j)! + 1);
+        } else {
+          map.set(j, 1);
+        }
+      });
+    });
+    return Array.from(map).map((i) => ({
+      value: i[0],
+      count: i[1],
+    }));
+  }, [data]);
+
+  if (!data) {
+    return (
+      <div className="relative h-80 w-full">
+        <CatLoading />
+      </div>
+    );
+  }
+
+  return (
+    <div className="mx-[5px] mb-5 animate-slide-up-big-in p-2 md:p-5">
+      <TagCloud
+        className="[&>*]:cursor-pointer"
+        minSize={18}
+        maxSize={36}
+        tags={tags}
+        onClick={(tag) => navigate(`/tag/${tag.value}`)}
+      />
+    </div>
+  );
 };
